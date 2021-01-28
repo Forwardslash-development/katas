9b2672b74b5c875e8857bab711d23f3dfc37c703
jv <jv@forwardslash-development.io>
2021-01-28 15:55:16 -0600

    (1 1 2 3 5 8)

---
+++ fibonacci/index.js
@@ -0,0 +1,19 @@
+//
+
+function generateFibonacci(howMany) {
+  let x = 1;
+  let y = 2;
+  let sequence = [x + y];
+
+  Array(howMany)
+    .fill()
+    .forEach(() => {
+      let sum = x + y;
+      sequence.push(sum);
+      x = y;
+      y = sum;
+    });
+  return sequence;
+}
+
+console.log(generateFibonacci(8));
