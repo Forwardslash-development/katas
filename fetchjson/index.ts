838fc50107e8b5c9a7433cf0c1e87f36f5d5c994
jv <jv@forwardslash-development.io>
2021-02-01 15:05:19 -0600

    added types

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -15,10 +15,15 @@ axios.get(url).then(response =>{
   const id = todo.id;
   const title = todo.title;
   const completed = todo.completed;
+
+  logTodo( id, title, completed);
   
+});
+
+const logTodo = (id: number, title: string, completed: boolean) => {
   console.log(`
   The ToDo with ID: ${id}
   Has a title of ${title}
   Is it finished? ${completed}
   `)
-})
+};
