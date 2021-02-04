da45b278649d1ff1df46bd2a799413b5c8a2675a
jv <jv@forwardslash-development.io>
2021-02-04 14:04:00 -0600

    TS kata

--- typeScript/2321/src/typeAnnotations.ts
+++ typeScript/2321/src/typeAnnotations.ts
@@ -29,3 +29,31 @@ let point: {x: number; y: number} = {
 const logNumber: (i: number) => void = (i: number) => {
   console.log(i);
 };
+
+// When to use annotations...
+// Functions that return the 'any' type, like JSON.parse.
+const json = '{"x": 10, "y": 20}';
+const cordinates: {"x": number; "y": number} = JSON.parse(json);
+console.log(cordinates);
+
+// when we declare a varible on one line 
+// and initalize it later
+
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+
+for (let i =0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+  }
+};
+
+// varibles whose type cannot be inferred
+let numbers = [-10,-1,12];
+let numberAboveZero: boolean | number = false;
+
+for (let i =0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}
